2f4ed7831862d790c8db8f1b024b05aaa206f34b
andreiboiardinov <cmcm.ab@gmail.com>
2023-08-05 12:28:18 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modalProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ecom admin",
+  description: "Ecommerce admin dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
